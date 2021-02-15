99f7d0e29d9607e52c8d47f1315bc8122b1a0165
Tomasz Tomasiak <tomasz.tomasiak.pl@gmail.com>
2021-02-15 12:39:54 +0100

    init

---
+++ src/test/java/com/rock_paper_scissors_lizard_spock/RockPaperScissorsLizardSpockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rock_paper_scissors_lizard_spock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RockPaperScissorsLizardSpockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
